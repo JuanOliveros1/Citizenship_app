27e353fea65e74204ef44ce5e8344c88d725e247
justBRENDAA <brendaa.scholar@gmail.com>
2024-03-08 01:28:11 -0600

    attached activity to buttons (review and home buttons)

--- app/src/main/java/com/example/citizenshipapp/MainActivity.java
+++ app/src/main/java/com/example/citizenshipapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.citizenshipapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void reviewOnClick(View v){
+        Intent i = new Intent(getApplicationContext(),activity_1.class);
+        startActivity(i);
     }
 
 }
