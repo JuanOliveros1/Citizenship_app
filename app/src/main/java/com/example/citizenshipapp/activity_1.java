a1451cd4fc59373f071375e69f7decc0303f3538
justBRENDAA <brendaa.scholar@gmail.com>
2024-03-08 01:12:57 -0600

    create question review activity

--- app/src/main/java/com/example/citizenshipapp/activity_1.java
+++ app/src/main/java/com/example/citizenshipapp/activity_1.java
@@ -3,16 +3,12 @@ package com.example.citizenshipapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class activity_1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_1);
     }
-
 }
