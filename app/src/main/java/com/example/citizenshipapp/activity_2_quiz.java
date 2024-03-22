99a6fe200a70abe64090c472c64c670332cc810c
justBRENDAA <brendaa.scholar@gmail.com>
2024-03-22 00:50:16 -0500

    add functions to quiz java

--- app/src/main/java/com/example/citizenshipapp/activity_2_quiz.java
+++ app/src/main/java/com/example/citizenshipapp/activity_2_quiz.java
@@ -5,18 +5,61 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class activity_2_quiz extends AppCompatActivity {
+public class activity_2_quiz extends AppCompatActivity implements View.OnClickListener{
+
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button ansA, ansB, ansC, ansD;
+    Button submitBtn;
+
+    int score = 0;
+    int totalQuestion = QuestionAnswer.question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer = "";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2_quiz);
+
+        totalQuestionsTextView = findViewById(R.id.total_questions);
+        questionTextView = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_A);
+        ansB = findViewById(R.id.ans_B);
+        ansC = findViewById(R.id.ans_C);
+        ansD = findViewById(R.id.ans_D);
+        submitBtn = findViewById(R.id.submit_button);
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        submitBtn.setOnClickListener(this);
+
+        totalQuestionsTextView.setText("Total questions : "+totalQuestion);
+
+        loadNewQuestion();
+
+
     }
 
     public void homeOnClick(View v){
         Intent i = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    void loadNewQuestion(){
+
     }
 }
 
