1c1b3c0afa42fa6405a97055facacdb7ffaeb694
justBRENDAA <brendaa.scholar@gmail.com>
2024-03-22 00:27:39 -0500

    Question arrays

---
+++ app/src/main/java/com/example/citizenshipapp/QuestionAnswer.java
@@ -0,0 +1,95 @@
+package com.example.citizenshipapp;
+public class QuestionAnswer {
+
+    public static String[] question = {
+            "What is the supreme law of the land?",    //01
+            "What does the Constitution do",    //02
+            "The idea of self-government is in the first three words of the Constitution. What are these words?",    //03
+            "What is an amendment?",    //04
+            "What do we call the first ten amendments to the Constitution?",    //05
+            "What is one right or freedom from the First Amendment?",    //06
+            "How many amendments does the Constitution have?",    //07
+            "What did the Declaration of Independence do?",    //08
+            "What are two rights in the Declaration of Independence?",    //09
+            "What is freedom of religion?",    //10
+            "What is the economic system in the United States?",    //11
+            "What is the “rule of law”?",    //12
+            "Name one branch or part of the government?",    //13
+            "What stops one branch of government from becoming too powerful?",    //14
+            "Who is in charge of the executive branch?",    //15
+            "Who makes federal laws?",    //16
+            "What are the two parts of the U.S. Congress?",    //17
+            "How many U.S. Senators are there?",    //18
+            "We elect a U.S. Senator for how many years?",    //19
+            "Who is one of your state’s U.S. Senators now?",    //20
+            "The House of Representatives has how many voting members?",    //21
+            "We elect a U.S. Representative for how many years?",    //22
+            "Name your U.S. Representative.",    //23
+            "Who does a U.S. Senator represent?",    //24
+            "Why do some states have more Representatives than other states?"    //25
+
+    };
+
+    public static String[][] choices = {
+            {"wrong","the Constitution","wrong","wrong"},    //01
+            {"protects basic rights of Americans","wrong","wrong","wrong"},    //02
+            {"We the People","wrong","wrong","wrong"},    //03
+            {"wrong","wrong","a change (to the Constitution)","wrong"},    //04
+            {"wrong","wrong","wrong","the Bill of Rights"},    //05
+            {"speech","wrong","wrong","wrong"},    //06
+            {"wrong","wrong","twenty-seven (27)","wrong"},    //07
+            {"wrong","wrong","wrong","declared our independence (from Great Britain)"},    //08
+            {"wrong","life and liberty","wrong","wrong"},    //09
+            {"You can practice any religion, or not practice a religion.","wrong","wrong","wrong"},    //10
+            {"wrong","wrong","market economy","wrong"},    //11
+            {"wrong","Everyone must follow the law","wrong","wrong"},    //12
+            {"wrong","legislative","wrong","wrong"},    //13
+            {"wrong","wrong","checks and balances","wrong"},    //14
+            {"the President","wrong","wrong","wrong"},    //15
+            {"wrong","Congress","wrong","wrong"},    //16
+            {"wrong","the Senate and House (of Representatives)","wrong","wrong"},    //17
+            {"wrong","wrong","wrong","one hundred (100)"},    //18
+            {"wrong","wrong","six (6)","wrong"},    //19
+            {"Answers will vary","wrong","wrong","wrong"},    //20
+            {"wrong","four hundred thirty-five (435)","wrong","wrong"},    //21
+            {"wrong","wrong","two (2)","wrong"},    //22
+            {"wrong","Answers will vary","wrong","wrong"},    //23
+            {"wrong","wrong","wrong","all people of the state"},    //24
+            {"wrong","(because of) the state’s population","wrong","wrong"}     //25
+    };
+
+    public static String[] correctAnswers = {
+            "the Constitution",    //01
+            "protects basic rights of Americans",    //02
+            "We the People",    //03
+            "a change (to the Constitution)",    //04
+            "the Bill of Rights",    //05
+            "speech",    //06
+            "twenty-seven (27)",    //07
+            "declared our independence (from Great Britain)",    //08
+            "life and liberty",    //09
+            "You can practice any religion, or not practice a religion",    //10
+            "market economy",    //11
+            "Everyone must follow the law",    //12
+            "legislative",    //13
+            "checks and balances",    //14
+            "the President",    //15
+            "Congress",    //16
+            "the Senate and House (of Representatives)",    //17
+            "one hundred (100)",    //18
+            "six (6)",    //19
+            "Answers will vary",    //20
+            "four hundred thirty-five (435)",    //21
+            "two (2)",    //22
+            "Answers will vary",    //23
+            "all people of the state",    //24
+            "(because of) the state’s population"    //25
+
+    };
+
+
+
+
+
+}
+
